19ad11ad631bb8c9a1ba0f9af4bcb681610e1647
seyahlater05 <seyahlater05@gmail.com>
2025-04-20 15:18:16 -0400

    starting ui

--- main.cpp
+++ main.cpp
@@ -53,15 +53,27 @@ void toggle_callback(Fl_Widget*, void*) {
 
 
 int main(int argc, char** argv) {
+    ifstream file("dataset.csv");
+    if (!file) {
+        cerr << "Could not open dataset.csv\n";
+        return 1;
+    }
+    string genre;
+    Graph graph;
+    graph.LoadGenreFromCSV(file, genre); // Only loads songs from that genre
+    vector<string> genre_list = graph.GetAllGenres();
+
+    const auto& songs = graph.GetSongsByGenre(genre);
+    const auto& matrix = graph.GetAdjacencyMatrix(genre);
     // Create main window
     Fl_Window* window = new Fl_Window(400, 300, "Control Panel");
     window->color(FL_WHITE);
 
     // Create drop-down menu
     menu_choice = new Fl_Choice(20, 20, 150, 30, "Options:");
-    menu_choice->add("Option 1");
-    menu_choice->add("Option 2");
-    menu_choice->add("Option 3");
+    for(string s : genre_list) {
+        menu_choice->add(s.c_str());
+    }
     menu_choice->value(0);  // Set default selection
 
     // Create start button
@@ -90,59 +102,54 @@ int main(int argc, char** argv) {
     window->end();
     window->show();
     return Fl::run();
-
-    string genre;
-    cout << "Enter a genre: ";
-    getline(cin, genre);
-
-    ifstream file("dataset.csv");
-    if (!file) {
-        cerr << "Could not open dataset.csv\n";
-        return 1;
-    }
-
-    Graph graph;
-    graph.LoadGenreFromCSV(file, genre); // Only loads songs from that genre
-
-    const auto& songs = graph.GetSongsByGenre(genre);
-    const auto& matrix = graph.GetAdjacencyMatrix(genre);
-
-    if (songs.size() < 10) {
-        cout << "Not enough songs in this genre to find 10 closest.\n";
-        return 1;
-    }
-
-    // Pick a random song
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dist(0, songs.size() - 1);
-    int startIndex = dist(gen);
-
-    cout << "\nStarting song: " << songs[startIndex].track_name << " by " << songs[startIndex].artists << "\n\n";
-
-    // Set to track unique recommendations by name+artist
-    set<string> seen;
-    seen.insert(songs[startIndex].track_name + songs[startIndex].artists);
-
-    vector<pair<float, int>> distances;
-    for (size_t i = 0; i < matrix[startIndex].size(); ++i) {
-        if (i != static_cast<size_t>(startIndex)) {
-            distances.emplace_back(matrix[startIndex][i], i);
-        }
-    }
-
-    sort(distances.begin(), distances.end());
-
-    cout << "9 closest songs:\n";
-    int count = 0;
-    for (const auto& [dist, index] : distances) {
-        string key = songs[index].track_name + songs[index].artists;
-        if (seen.count(key)) continue;
-
-        seen.insert(key);
-        cout << " - " << songs[index].track_name << " by " << songs[index].artists << "\n";
-        if (++count == 9) break;
-    }
-
-    return 0;
+    //
+    // cout << "Enter a genre: ";
+    // getline(cin, genre);
+    //
+    //
+    //
+    // Graph graph;
+    // graph.LoadGenreFromCSV(file, genre); // Only loads songs from that genre
+    //
+    // const auto& songs = graph.GetSongsByGenre(genre);
+    // const auto& matrix = graph.GetAdjacencyMatrix(genre);
+    //
+    // if (songs.size() < 10) {
+    //     cout << "Not enough songs in this genre to find 10 closest.\n";
+    //     return 1;
+    // }
+    //
+    // // Pick a random song
+    // random_device rd;
+    // mt19937 gen(rd());
+    // uniform_int_distribution<> dist(0, songs.size() - 1);
+    // int startIndex = dist(gen);
+    //
+    // cout << "\nStarting song: " << songs[startIndex].track_name << " by " << songs[startIndex].artists << "\n\n";
+    //
+    // // Set to track unique recommendations by name+artist
+    // set<string> seen;
+    // seen.insert(songs[startIndex].track_name + songs[startIndex].artists);
+    //
+    // vector<pair<float, int>> distances;
+    // for (size_t i = 0; i < matrix[startIndex].size(); ++i) {
+    //     if (i != static_cast<size_t>(startIndex)) {
+    //         distances.emplace_back(matrix[startIndex][i], i);
+    //     }
+    // }
+    //
+    // sort(distances.begin(), distances.end());
+    //
+    // cout << "9 closest songs:\n";
+    // int count = 0;
+    // for (const auto& [dist, index] : distances) {
+    //     string key = songs[index].track_name + songs[index].artists;
+    //     if (seen.count(key)) continue;
+    //
+    //     seen.insert(key);
+    //     cout << " - " << songs[index].track_name << " by " << songs[index].artists << "\n";
+    //     if (++count == 9) break;
+    // }
+    //
+    // return 0;
 }
